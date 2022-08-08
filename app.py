0a15c422ee9f5075bb17b50f065e33e067f4d635
Sebastiaan Nolte <sebas.nolte@hotmail.com>
2022-08-08 20:27:45 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,282 @@
+"""Encode url"""
+from urllib.parse import urlencode
+import re
+import os
+import urllib.request
+from functools import wraps
+from flask_login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    login_required,
+    current_user,
+)
+from flask_cors import CORS
+from flask import Flask, request, redirect, json, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import requests
+from sqlalchemy import (
+    create_engine,
+    text,
+)
+
+
+app = Flask(__name__)
+cors = CORS(app)
+
+app.secret_key = os.environ["SECRET_KEY"]
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Load the user for Flask-login
+
+    Args:
+        user_id (Integer): The user_id to load
+
+    Returns:
+        User: User model
+    """
+    return User.query.get(user_id)
+
+
+DATABASE_URI = "postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}".format(
+    dbuser=os.environ["DB_USER"],
+    dbpass=os.environ["DB_PASS"],
+    dbhost=os.environ["DB_HOST"],
+    dbname=os.environ["DB_NAME"],
+)
+engine = create_engine(DATABASE_URI)
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=DATABASE_URI,
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+# Initialize the database connection
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+steam_id_re = re.compile("https://steamcommunity.com/openid/id/(.*?)$")
+# Create databases, if databases exists doesn't issue create
+# For schema changes, run "flask db migrate"
+from models import User, Votes, Games, VotesSchema
+
+db.create_all()
+db.session.commit()
+
+
+def get_user_info(steam_id):
+    """Get user info from the steam api
+
+    Args:
+        steam_id (Integer): Steam ID of the user
+
+    Returns:
+        Object: Steam user
+    """
+    options = {"key": os.environ["STEAM_API_KEY"], "steamids": steam_id}
+    url = (
+        "https://api.steampowered.com/ISteamUser/"
+        "GetPlayerSummaries/v0002/?%s" % urllib.parse.urlencode(options)
+    )
+
+    response = json.load(urllib.request.urlopen(url))
+    return response["response"]["players"][0] or {}
+
+
+@app.route("/auth")
+def auth_with_steam():
+    """Authenticate an user via Steam
+
+    Returns:
+        redirect: Redirect to the Steam login page
+    """
+    steam_openid_url = "https://steamcommunity.com/openid/login"
+    params = {
+        "openid.ns": "http://specs.openid.net/auth/2.0",
+        "openid.identity": "http://specs.openid.net/auth/2.0/identifier_select",
+        "openid.claimed_id": "http://specs.openid.net/auth/2.0/identifier_select",
+        "openid.mode": "checkid_setup",
+        "openid.return_to": os.environ["URL"] + "/authorize",
+        "openid.realm": os.environ["URL"],
+    }
+
+    query_string = urlencode(params)
+    auth_url = steam_openid_url + "?" + query_string
+
+    return redirect(auth_url)
+
+
+@app.route("/authorize")
+def authorize():
+    """Verify the Steam user and login
+
+    Returns:
+        String: Loged in info message
+    """
+    match = steam_id_re.search(dict(request.args)["openid.identity"])
+    db_user = User.get_or_create(match.group(1))
+    steam_data = get_user_info(db_user.steam_id)
+    db_user.nickname = steam_data["personaname"]
+    db.session.commit()
+    login_user(user=db_user, remember=True)
+    return (
+        "You are logged in as "
+        + steam_data["personaname"]
+        + ". Refresh the Steam page to start voting!"
+    )
+
+
+@app.route("/user")
+@login_required
+def user():
+    """Get user info and check if authenticated
+
+    Returns:
+        Object: is_authenticated and optional the name
+    """
+    print(os.environ['SECRET_KEY'], flush=True)
+    if current_user.is_authenticated:
+        return {"is_authenticated": True, "name": current_user.nickname}
+    return {"is_authenticated": False}
+
+
+@app.route("/vote", methods=["POST"])
+@login_required
+def vote():
+    """User can vote
+
+    Returns:
+        Object: Return votes summary
+    """
+    data = request.get_json()
+    user_vote = False
+    if current_user.is_authenticated:
+        data["user_id"] = current_user.id
+        Votes.update_or_create(data)
+
+        # Copied from votes
+        mapping = {1: "kb", 2: "controller"}
+        sql = text(
+            "SELECT vote, count(vote) from votes where app_id ="
+            + data["app_id"]
+            + "group by vote"
+        )
+        results = engine.execute(sql)
+        voters = {mapping[1]: 0, mapping[2]: 0}
+        total = 0
+        for key, value in results:
+            voters[mapping[key]] = value
+            total = total + value
+
+        user_vote = (
+            Votes.query.filter_by(user_id=current_user.id)
+            .filter_by(app_id=data["app_id"])
+            .first()
+        )
+        if user_vote:
+            vote_schema = VotesSchema()
+            user_vote = vote_schema.dump(user_vote)
+            user_vote = mapping[user_vote["vote"]]
+
+        return {
+            "result": voters,
+            "total": total,
+            "user_vote": user_vote,
+            "is_authenticated": current_user.is_authenticated,
+        }
+    return {"success": False}
+
+
+@app.route("/votes/<steam_id>")
+def votes(steam_id):
+    """Votes of a specific game
+
+    Args:
+        steam_id (Integer): Steam game id
+
+    Returns:
+        Object: Return votes summary
+    """
+
+    mapping = {1: "kb", 2: "controller"}
+    game = Games.query.get(steam_id)
+    if game is None:
+        return {"error": "This game in not indexed or available to vote"}
+
+    sql = text(
+        "SELECT vote, count(vote) from votes where app_id ="
+        + steam_id
+        + "group by vote"
+    )
+    results = engine.execute(sql)
+    voters = {mapping[1]: 0, mapping[2]: 0}
+    total = 0
+    for key, value in results:
+        voters[mapping[key]] = value
+        total = total + value
+    user_vote = False
+    if current_user.is_authenticated:
+        user_vote = (
+            Votes.query.filter_by(user_id=current_user.id)
+            .filter_by(app_id=steam_id)
+            .first()
+        )
+        vote_schema = VotesSchema()
+        user_vote = vote_schema.dump(user_vote)
+        if user_vote:
+            user_vote = mapping[user_vote["vote"]]
+
+    return {
+        "result": voters,
+        "total": total,
+        "user_vote": user_vote,
+        "is_authenticated": current_user.is_authenticated,
+    }
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    """Logout the current user
+
+    Returns:
+        Object: Status of user
+    """
+    logout_user()
+    if current_user.is_authenticated:
+        return {"success": False}
+    return {"success": True}
+
+
+def admin_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if current_user.is_admin is True:
+            return f(*args, **kwargs)
+        else:
+            return abort(401)
+
+    return wrap
+
+
+@app.route("/update-games")
+@admin_required
+def update_games():
+
+    """Update or create Steam games in DB
+
+    Returns:
+        String: done message
+    """
+
+    url = "http://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json"
+    steam_request = requests.get(url=url, params={})
+    data = steam_request.json()
+    for steam_game in data["applist"]["apps"]:
+        Games.update_or_create(steam_game)
+
+    return "Games updated"
---
+++ models.py
@@ -0,0 +1,167 @@
+""" DB """
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    DateTime,
+    BigInteger,
+    func,
+    Boolean,
+)
+from sqlalchemy.orm import declarative_base, relationship
+from flask_marshmallow import Marshmallow
+from flask_login import UserMixin
+from app import db, app
+
+Base = declarative_base()
+
+
+class Games(db.Model):
+    """Games model
+
+    Args:
+        db (Model): model
+    """
+
+    __tablename__ = "games"
+    app_id = Column(Integer, primary_key=True)
+    name = Column(String(500))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    votes = relationship("Votes", backref="games", passive_deletes=True)
+
+    @staticmethod
+    def update_or_create(data):
+        """update or create a game
+
+        Args:
+            data (Object): Object with type, appid and name
+        """
+        game = Games.query.get(data["appid"])
+
+        if game is None:
+            new_game = Games()
+            new_game.app_id = data["appid"]
+            new_game.name = data["name"]
+            db.session.add(new_game)
+            db.session.commit()
+        else:
+            game.name = data["name"]
+            db.session.commit()
+
+
+class User(db.Model, UserMixin):
+    """User model
+
+    Args:
+        db (Model): model
+    """
+
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    steam_id = Column(String(100), unique=True)
+    nickname = Column(String(1000))
+    is_admin = Column(Boolean, default=0)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    votes = relationship("Votes", backref="user", passive_deletes=True)
+
+    @staticmethod
+    def get_or_create(steam_id):
+        """Return the user or create a new one based on the steam_id
+
+        Args:
+            steam_id (Integer): Steam ID
+
+        Returns:
+            User: User model
+        """
+        user = User.query.filter_by(steam_id=steam_id).first()
+        if user is None:
+            user = User()
+            user.steam_id = steam_id
+            db.session.add(user)
+        return user
+
+
+class Votes(db.Model):
+    """Votes model
+
+    Args:
+        db (Model): model
+    """
+
+    __tablename__ = "votes"
+    __table_args__ = (
+        db.UniqueConstraint("app_id", "user_id", name="unique_votes_app_id_user_id"),
+    )
+    id = Column(Integer, primary_key=True)
+    app_id = Column(
+        Integer, ForeignKey("games.app_id", ondelete="CASCADE"), nullable=False
+    )
+    user_id = Column(
+        BigInteger, ForeignKey("user.id", ondelete="CASCADE"), nullable=False
+    )
+    vote = Column(Integer, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    @staticmethod
+    def update_or_create(data):
+        """Update the user if already voted otherwise create a vote.
+        If the vote is the same, delete it
+
+        Args:
+            data (Object): Object with type, app_id and user_id
+        """
+        vote = (
+            Votes.query.filter_by(user_id=data["user_id"])
+            .filter_by(app_id=data["app_id"])
+            .first()
+        )
+
+        if vote is None:
+            new_vote = Votes()
+            new_vote.vote = data["type"]
+            new_vote.app_id = data["app_id"]
+            new_vote.user_id = data["user_id"]
+            db.session.add(new_vote)
+            db.session.commit()
+        else:
+            if vote.vote == data["type"]:
+                db.session.delete(vote)
+                db.session.commit()
+            else:
+                vote.vote = data["type"]
+                db.session.commit()
+
+
+ma = Marshmallow(app)
+
+
+class GameSchema(ma.SQLAlchemyAutoSchema):
+    """Schema to return output as JSOn"""
+
+    class Meta:
+        """Games model"""
+
+        model = Games
+
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    """Schema to return output as JSOn"""
+
+    class Meta:
+        """User model"""
+
+        model = User
+
+
+class VotesSchema(ma.SQLAlchemyAutoSchema):
+    """Schema to return output as JSOn"""
+
+    class Meta:
+        """Votes model"""
+
+        model = Votes
