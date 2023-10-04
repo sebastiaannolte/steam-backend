0d5cf4d25c1965b045ec642ea971f587e676d889
Sebastiaan <sebas.nolte@hotmail.com>
2023-10-04 22:48:10 +0200

    Reverse steam games call

--- app/routes.py
+++ app/routes.py
@@ -220,7 +220,7 @@ def update_games():
     url = "http://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json"
     steam_request = requests.get(url=url, params={})
     data = steam_request.json()
-    for steam_game in data["applist"]["apps"]:
+    for steam_game in reversed(data["applist"]["apps"]):
         Games.update_or_create(steam_game)
 
     return "Games updated"
