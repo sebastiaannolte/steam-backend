da1d5cef555bbf48e63bde5c3e905360b601b64b
Sebastiaan <sebas.nolte@hotmail.com>
2024-08-01 22:33:08 +0200

    Test

--- updategames.py
+++ updategames.py
@@ -1,5 +1,11 @@
 
-from app.updategames import update_games
-from app import app
-with app.app_context():
-    update_games()
+# from app.updategames import update_games
+# from app import app
+# with app.app_context():
+#     update_games()
+
+import logging
+logging.info("Games updated")
+print("Games updated")
+from dotenv import load_dotenv
+load_dotenv()
